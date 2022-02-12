498f3843f18fc7b9c100b3897072b9ab589707df
Manjusha Jaiswal <manjushamob@gmail.com>
2022-02-12 01:06:35 -0600

    changed code

--- src/main/java/variousConcepts/GitTest2.java
+++ src/main/java/variousConcepts/GitTest2.java
@@ -3,12 +3,14 @@ package variousConcepts;
 public class GitTest2 {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("git test");
-		
 
 		System.out.println("git test 1");
-		
-		
+
+		System.out.println("git test 2");
+
+		System.out.println("git test 3");
+
 	}
 }
